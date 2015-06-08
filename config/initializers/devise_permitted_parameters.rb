2c5085e3902a991d770ce706760f6cd02779a521
Alexander Nikulin <nikulin.alexander@yahoo.com>
2015-06-08 15:11:18 -0700

    Fixed sign up unpermitted parameter issue

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,7 +8,10 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) { |u| 
+                u.permit(:email, :password, :password_confirmation, :name) 
+             }
+    #devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
     #devise_parameter_sanitizer.for(:sign_up) << :name { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
     #devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name) }
