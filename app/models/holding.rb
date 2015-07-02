b60b569829704b32c4d3429c2d9d735cceba3a70
Alexander Nikulin <nikulin.alexander@yahoo.com>
2015-07-02 14:12:42 -0700

    Added model validation for new holdings

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -2,5 +2,5 @@ class Holding < ActiveRecord::Base
   #belongs_to :user
   belongs_to :stock
   belongs_to :portfolio, :inverse_of => :holdings
-  #validates :name, presence: true  
+  validates :stock_id, :portfolio_id, :amount, presence: true  
 end
