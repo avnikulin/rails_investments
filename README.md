6cd8b9d0ae493db2b080712eb3b5cf77a99355b9
Alexander Nikulin <nikulin.alexander@yahoo.com>
2015-07-13 22:15:19 -0700

    Added a delete link for every holding in the add/edit form and updated readme

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Rails Investments
 This is a Ruby on Rails project for investment porftolio analytics.
 
 Demo: https://infinite-dusk-1140.herokuapp.com/
+
+Username: guest@example.com
+Password: password
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,6 @@
 	$(document).ready( function() {
 	
 	$('.datepicker').datepicker({format: 'yyyy-mm-dd'});
-	$('.datepicker2').datepicker({format: 'yyyy-mm-dd'});
 	
 
 	$("td.currency").each(function() {
